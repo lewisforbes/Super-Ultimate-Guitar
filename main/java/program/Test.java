7233505f27282dce5f5b710104a33c9ee71d3a0d
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2020-05-17 22:00:45 +0100

    Delete Test.java

--- main/java/program/Test.java
+++
@@ -1,8 +0,0 @@
-package program;
-
-public class Test {
-    public static void main(String[] args) {
-        String song = Main.newSong("south of the river tom misch");
-        System.out.println(song);
-    }
-}
